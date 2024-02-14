caeb431d1cddfaa2ee71cdacb0b08907c694f09e
tod1az <t.diaz.soto@gmail.com>
2024-02-14 18:29:32 -0300

    add reference filter

--- src/components/productsList.tsx
+++ src/components/productsList.tsx
@@ -1,12 +1,36 @@
 "use client";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { useState } from "react";
+import { references, type Reference } from "../lib/data";
 
 export default function ProductList() {
-  const dbProducts = useQuery(api.products.get);
-  if (dbProducts?.length === 0) return <h1>No hay productos registrados</h1>;
+  const [reference, setReference] = useState<Reference>();
+  const dbProducts = useQuery(api.products.getWithFilters, {
+    reference: reference,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setReference(e.target.value as Reference);
+  };
   return (
     <section className="flex flex-col gap-6 items-center justify-center">
+      <select
+        onChange={handleChange}
+        value={reference}
+        className="bg-slate-600"
+      >
+        <option disabled selected>
+          Referencia
+        </option>
+        {Object.values(references).map((ref) => {
+          return (
+            <option value={ref} key={String(ref)}>
+              {ref}
+            </option>
+          );
+        })}
+      </select>
       <h1 className="text-6xl">Products</h1>
       <section className="w-[40rem]">
         <header className="px-2 grid grid-cols-5 justify-between">
