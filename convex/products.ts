8e517ed3cf251729743db9f8a4de6a9aa410c046
tod1az <t.diaz.soto@gmail.com>
2024-02-14 18:29:20 -0300

    add type Product add getWithFilters query

--- convex/products.ts
+++ convex/products.ts
@@ -1,6 +1,15 @@
+import { Id } from "./_generated/dataModel";
 import { mutation } from "./_generated/server";
 import { query } from "./_generated/server";
 import { v } from "convex/values";
+
+type Product = {
+  _id: Id<"products">;
+  _creationTime: number;
+  stock?: number | undefined;
+  reference: string;
+  name: string;
+};
 
 export const createProduct = mutation({
   args: {
@@ -17,9 +26,31 @@ export const createProduct = mutation({
   },
 });
 
-export const get = query({
+export const getAll = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("products").collect();
+  },
+});
+
+export const getWithFilters = query({
+  args: {
+    query: v.optional(v.string()),
+    reference: v.optional(v.string()),
+  },
+  handler: async (ctx, { query, reference }) => {
+    const result: Product[] = [];
+
+    if (query) {
+      const queryResults = await ctx.db.query("products").collect();
+    }
+    if (reference) {
+      const referenceResult = await ctx.db
+        .query("products")
+        .filter((q) => q.eq(q.field("reference"), reference))
+        .collect();
+      result.push(...referenceResult);
+    }
+    return result;
   },
 });
